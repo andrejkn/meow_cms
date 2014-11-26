b1fcf0ecfa57a95a29db0e51d84d8dec6518f75a
andrej <andrej@eidosearch.com>
2014-11-26 18:13:34 -0500

    fixed collection insertion

--- client/templates/admin-portal/posts-list.js
+++ client/templates/admin-portal/posts-list.js
@@ -4,6 +4,21 @@ if (Meteor.isClient) {
     Template.posts_list.helpers({
         posts: function () {
             return Posts.find({});
+        },
+
+        formatDate: function (date) {
+            console.dir(date);
+            var formattedDate = date.getDay() + '/' +
+                date.getMonth() + '/' +
+                date.getFullYear() + ' ' +
+                date.getHours() + ':' +
+                date.getMinutes() + ':' +
+                date.getSeconds();
+            return formattedDate;
+        },
+
+        concatenate: function(content, numCharacters) {
+            return content.substr(0, numCharacters);
         }
     });
 }
