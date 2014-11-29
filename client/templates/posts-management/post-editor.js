9bb9f59aacd916fa4830cf2086a89530f701bbae
andrej <andrej@eidosearch.com>
2014-11-29 10:17:34 -0500

    changed folder structure

--- client/templates/posts-management/post-editor.js
+++ client/templates/posts-management/post-editor.js
@@ -1,65 +1,59 @@
 
 if (Meteor.isClient) {
 
-  Meteor.startup(function () {
-    Session.set('editorMode', 'text');
-  });
+	Meteor.startup(function () {
+		Session.set('editorMode', 'text');
+	});
 
-  Template.post_editor.helpers({
-    isActive: function (editorMode) {
-      return Session.get('editorMode') === editorMode;
-    }
-  });
+	Template.post_editor.helpers({
+		isActive: function (editorMode) {
+			return Session.get('editorMode') === editorMode;
+		}
+	});
 
-  Template.post_editor.events({
-    'click #save_post': function () {
+	Template.post_editor.events({
+		'click #save_post': function () {
 
-      // increment the counter when button is clicked
-      var subject = $('#post_subject_holder').val(),
-          content = $('#post_content_holder').html(),
-          user = 'andrej',
-          editId = Session.get('id-of-post-to-be-edited');
+			// increment the counter when button is clicked
+			var subject = $('#post_subject_holder').val(),
+				content = $('#post_content_holder').html(),
+				user = 'andrej',
+				editId = Session.get('id-of-post-to-be-edited');
 
-      if (!_.isEmpty(user) && !_.isEmpty(content)) {
+			if (!_.isEmpty(user) && !_.isEmpty(content)) {
 
-        // if in EDIT mode
-        if (editId) {
-          Posts.update({
-            _id: editId
-          }, {
-            $set: {
-              content: content,
-              subject: subject,
-              editedAt: new Date()
-            }
-          });
-          Session.set('id-of-post-to-be-edited', undefined);
-        } else {
-          Posts.insert({
-            createdBy: user,
-            createdAt: new Date(),
-            subject: subject,
-            content: content,
-            visible: false,
-            editedAt: null
-          });
-        }
-      }
-    },
+				// if in EDIT mode
+				if (editId) {
+					Posts.update({
+					_id: editId
+					}, {
+					$set: {
+						content: content,
+						subject: subject,
+						editedAt: new Date()
+					}
+					});
+					Session.set('id-of-post-to-be-edited', undefined);
+				} else {
+					Posts.insert({
+					createdBy: user,
+					createdAt: new Date(),
+					subject: subject,
+					content: content,
+					visible: false,
+					editedAt: null
+					});
+				}
+			}
+		},
 
-    'click #text_mode' : function () {
-      Session.set('editorMode', 'text');
-    },
+		'click #text_mode' : function () {
+			Session.set('editorMode', 'text');
+		},
 
-    'click #html_mode' : function () {
-      Session.set('editorMode', 'html');
-    }
+		'click #html_mode' : function () {
+			Session.set('editorMode', 'html');
+		}
 
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	});
 }
--- client/templates/posts-management/posts-list.js
+++ client/templates/posts-management/posts-list.js
@@ -1,67 +1,62 @@
 
 if (Meteor.isClient) {
 
-    // This code only runs on the client
-    Template.posts_list.helpers({
-        posts: function () {
-            var count = 1,
-                posts = Posts.find();
+	// This code only runs on the client
+	Template.posts_list.helpers({
+		posts: function () {
+			var count = 1,
+				posts = Posts.find();
 
-            return posts.map(function (post, index, cursor) {
-                post.index = count;
-                count += 1;
-                return post;
-            });
-        },
+			return posts.map(function (post, index, cursor) {
+				post.index = count;
+				count += 1;
+				return post;
+			});
+		},
 
-        formatDate: function (date) {
-            if (_.isNull(date) || _.isUndefined(date)) {
-                return '...';
-            }
+		formatDate: function (date) {
+			if (_.isNull(date) || _.isUndefined(date)) {
+				return '...';
+			}
 
-            var formattedDate = date.getDay() + '/' +
-                date.getMonth() + '/' +
-                date.getFullYear() + ' ' +
-                date.getHours() + ':' +
-                date.getMinutes() + ':' +
-                date.getSeconds();
-            return formattedDate;
-        },
+			var formattedDate = date.getDay() + '/' +
+				date.getMonth() + '/' +
+				date.getFullYear() + ' ' +
+				date.getHours() + ':' +
+				date.getMinutes() + ':' +
+				date.getSeconds();
+			return formattedDate;
+		},
 
-        concatenate: function(content, numCharacters) {
-            return content.substr(0, numCharacters) + '...';
-        }
-    });
+		concatenate: function(content, numCharacters) {
+			return content.substr(0, numCharacters) + '...';
+		}
+	});
 
-    Template.posts_list.events({
-        'click .post_content_list': function () {
-            var selectedContent = '#post_content_tooltip_' + this.index;
-            $(selectedContent).show();
+	Template.posts_list.events({
+		'click .post_content_list': function () {
+			var selectedContent = '#post_content_tooltip_' + this.index;
+			$(selectedContent).show();
 
-            $('.post_content_list, .post_content_tooltip_remove').click( function() {
-                $('.meow_hidden').hide();
-            });
+			$('.post_content_list, .post_content_tooltip_remove').click( function() {
+				$('.meow_hidden').hide();
+			});
 
-        },
+		},
 
-        //'click body': function () {
-        //    var selectedContent = '#post_content_tooltip_' + this.index;
-        //    $(selectedContent).hide();
-        //},
+		'click .remove_post': function () {
+			Posts.remove(this._id);
+		},
 
-        'click .remove_post': function () {
-            Posts.remove(this._id);
-        },
+		'click .edit_post': function () {
+			Session.set('id-of-post-to-be-edited', this._id);
 
-        'click .edit_post': function () {
-            Session.set('id-of-post-to-be-edited', this._id);
+			// load the post that will be edited in the editor
+			var subjectSelector = $('#post_subject_holder'),
+				contentSelector = $('#post_content_holder');
 
-            // load the post that will be edited in the editor
-            var subjectSelector = $('#post_subject_holder'),
-                contentSelector = $('#post_content_holder');
-
-            subjectSelector.val(this.subject);
-            contentSelector.html(this.content);
-        }
-    });
+			subjectSelector.val(this.subject);
+			contentSelector.html(this.content);
+		}
+	});
 }
